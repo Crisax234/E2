e311e9d49776f81da4e651562dfb3d729f2aa916
cristobalcarvallo <cristobal.carvallo@uc.cl>
2022-11-06 00:09:41 -0300

    app, con post y get basicos, por implementar logica del servidor

--- src/app.js
+++ src/app.js
@@ -34,15 +34,16 @@ router.get("user.show", "/user", async (ctx) => {
 });
 
 
-router.post('solicitud.create', '/solicitud', async (ctx) => {
+router.post('cars.create', '/cars', async (ctx) => {
     try {
 
-        const solicitud = await ctx.orm.Solicitud.create({
-        titulo: ctx.request.body.titulo,
-        presupuesto: ctx.request.body.presupuesto,
+        const cars = await ctx.orm.Cars.create({
         chasis: ctx.request.body.chasis,
-        descripcion: ctx.request.body.descripcion,
-        estado: ctx.request.body.estado
+        marca: ctx.request.body.marca,
+        modelo: ctx.request.body.modelo,
+        hp: ctx.request.body.hp,
+        torque: ctx.request.body.torque,
+        peso: ctx.request.body.peso
         });
         ctx.status = 201;
     } catch (error) {
@@ -51,15 +52,106 @@ router.post('solicitud.create', '/solicitud', async (ctx) => {
   })
 
 
-router.get("solicitud.show", "/solicitud", async (ctx) => {
+router.get("cars.show", "/cars", async (ctx) => {
     try {
-        const solicitudes = await ctx.orm.Solicitud.findAll();
-        ctx.body = solicitudes;
+        const cars = await ctx.orm.Cars.findAll();
+        ctx.body = cars;
       } catch (error) {
         console.log(error);
         ctx.throw(404);
       }
 });
+
+
+
+router.post('solicitud.create', '/solicitud', async (ctx) => {
+  try {
+
+      const solicitud = await ctx.orm.Solicitud.create({
+      titulo: ctx.request.body.titulo,
+      presupuesto: ctx.request.body.presupuesto,
+      chasis: ctx.request.body.chasis,
+      descripcion: ctx.request.body.descripcion,
+      estado: ctx.request.body.estado
+      });
+      ctx.status = 201;
+  } catch (error) {
+    ctx.throw(error);
+  }
+})
+
+
+router.get("solicitud.show", "/solicitud", async (ctx) => {
+  try {
+      const solicitudes = await ctx.orm.Solicitud.findAll();
+      ctx.body = solicitudes;
+    } catch (error) {
+      console.log(error);
+      ctx.throw(404);
+    }
+});
+
+router.post('simulacion.create', '/simulacion', async (ctx) => {
+  try {
+
+      const simulacion = await ctx.orm.Simulacion.create({
+      id_solicitud: ctx.request.body.id_solicitud,
+      chasis: ctx.request.body.chasis,
+      hp: ctx.request.body.hp,
+      torque: ctx.request.body.torque,
+      costo: ctx.request.body.costo,
+      estado: ctx.request.body.estado
+      });
+      ctx.status = 201;
+  } catch (error) {
+    ctx.throw(error);
+  }
+})
+
+
+router.get("simulacion.show", "/simulacion", async (ctx) => {
+  try {
+      const simulaciones = await ctx.orm.Simulacion.findAll();
+      ctx.body = simulaciones;
+    } catch (error) {
+      console.log(error);
+      ctx.throw(404);
+    }
+});
+
+
+router.post('respuesta.create', '/respuesta', async (ctx) => {
+  try {
+
+      const respuesta = await ctx.orm.respuesta.create({
+      id_solicitud: ctx.request.body.id_solicitud,
+      id_simulacion: ctx.request.body.id_simulacion,
+      hp: ctx.request.body.hp,
+      torque: ctx.request.body.torque,
+      estetica: ctx.request.body.estetica,
+      costo: ctx.request.body.costo
+      });
+      ctx.status = 201;
+  } catch (error) {
+    ctx.throw(error);
+  }
+})
+
+
+router.get("respuesta.show", "/respuesta", async (ctx) => {
+  try {
+      const respuestas = await ctx.orm.Respuesta.findAll();
+      ctx.body = respuestas;
+    } catch (error) {
+      console.log(error);
+      ctx.throw(404);
+    }
+});
+
+
+
+
+
 
 app.use(cors({ origin: '*' }))
 app.context.orm = orm;
