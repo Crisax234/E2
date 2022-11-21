143849e876f6759cd0ec27eebb773a55d43e0f82
cristobalcarvallo <cristobal.carvallo@uc.cl>
2022-11-20 23:20:27 -0300

    endpoints

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router');
 
 const router = new Router();
-router.get('dashboard.show', '/', async (ctx) => {
+router.get('dashboard.show', '/cantidad', async (ctx) => {
     try {
         const simulaciones = await ctx.orm.Simulacion.findAll();
         const cant_simulaciones = simulaciones.length;
@@ -17,8 +17,30 @@ router.get('dashboard.show', '/', async (ctx) => {
         }
         ctx.body = [
             {name : "cant_simulaciones", pv : cant_simulaciones},
-            {name : "costo_total_simulaciones", pv : costo_total_simulaciones},
             {name : "cant_solicitudes", pv : cant_solicitudes},
+  
+        ];
+    } catch (error) {
+        console.log(error);
+        ctx.throw(404);
+    }
+});
+router.get('dashboard.show', '/costo', async (ctx) => {
+    try {
+        const simulaciones = await ctx.orm.Simulacion.findAll();
+        const cant_simulaciones = simulaciones.length;
+        var costo_total_simulaciones = 0;
+        for (let step = 0; step < cant_simulaciones; step++) {
+            costo_total_simulaciones += simulaciones[step].costo;
+        }
+        const solicitudes = await ctx.orm.Solicitud.findAll();
+        const cant_solicitudes = solicitudes.length;
+        var costo_total_solicitudes = 0;
+        for (let step = 0; step < cant_solicitudes; step++) {
+            costo_total_solicitudes += solicitudes[step].presupuesto;
+        }
+        ctx.body = [
+            {name : "costo_total_simulaciones", pv : costo_total_simulaciones},
             {name : "costo_total_solicitudes", pv: costo_total_solicitudes}
         ];
     } catch (error) {
