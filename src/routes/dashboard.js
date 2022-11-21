cf4abf648d7d8238cd189ea34b605e69a32ef44a
cristobalcarvallo <cristobal.carvallo@uc.cl>
2022-11-21 15:29:57 -0300

    version casi final backend

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -15,12 +15,49 @@ router.get('dashboard.show', '/', async (ctx) => {
         for (let step = 0; step < cant_solicitudes; step++) {
             costo_total_solicitudes += solicitudes[step].presupuesto;
         }
+        const piezasPerformance = await ctx.orm.Performance.findAll();
+        const cant_piezasPerformance = piezasPerformance.length;
+        const piezasLook = await ctx.orm.Look.findAll();
+        const cant_piezasLook = piezasLook.length;
+        var categoriaIntercooler = 0;
+        var categoriaTurbo = 0;
+        var categoriaChargepipe = 0;
+        var categoriaCapot = 0;
+        var categoriaLlanta = 0;
+        var categoriaNeumatico = 0;
+        for (let step = 0; step < cant_piezasPerformance; step++) {
+            if (piezasPerformance[step].categoria == "Intercooler") {
+                categoriaIntercooler += 1;
+            }else if(piezasPerformance[step].categoria == "ChargePipe"){
+                categoriaChargepipe += 1;
+            }else if(piezasPerformance[step].categoria == "Turbo"){
+                categoriaTurbo += 1;
+            }
+        }     
+        for (let step = 0; step < cant_piezasLook; step++) {
+            if (piezasLook[step].categoria == "Capot") {
+                categoriaCapot += 1;
+            }else if(piezasLook[step].categoria == "Llanta"){
+                categoriaLlanta += 1;
+            }else if(piezasLook[step].categoria == "Neumatico"){
+                categoriaNeumatico += 1;
+            } 
+        }
+
         ctx.body = [
             [{name : "cant_simulaciones", pv : cant_simulaciones},
             {name : "cant_solicitudes", pv : cant_solicitudes}],[
 
             {name : "costo_total_simulaciones", pv : costo_total_simulaciones},
-            {name : "costo_total_solicitudes", pv: costo_total_solicitudes}]
+            {name : "costo_total_solicitudes", pv: costo_total_solicitudes}],
+
+            [{name : "categoriaIntercooler", pv : categoriaIntercooler},
+            {name : "categoriaTurbo", pv : categoriaTurbo},
+            {name : "categoriaChargepipe", pv : categoriaChargepipe}],
+            
+            [{name : "categoriaCapot", pv : categoriaCapot},
+            {name : "categoriaLlanta", pv : categoriaLlanta},
+            {name : "categoriaNeumatico", pv : categoriaNeumatico}]
         ];
     } catch (error) {
         console.log(error);
